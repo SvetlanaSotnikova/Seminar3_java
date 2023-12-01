b4523263fd49134572027870043f833f4e09c88e
Constantin Cutureanu <constantin.cutureanu@gmd.local>
2023-12-01 09:53:04 +0200

    save files

---
+++ Seminar3_Homework/Seminar3_Homework.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Seminar3_Homework {
+    public static void main(String[] args) {
+
+    }
+
+    static void task0() {
+        // Реализовать алгоритм сортировки слиянием
+
+    }
+
+    static void task1() {
+        // Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+    }
+
+    static void task2() {
+        // Задан целочисленный список ArrayList. Найти минимальное, максимальное и
+        // среднее из
+        // этого списка.
+
+    }
+}
